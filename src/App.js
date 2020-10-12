63d7d8ae255518d884d6cd9346521d18826c2b13
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2020-10-12 22:54:33 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import './App.css';
 
 import Header from './components/Header';
@@ -6,13 +6,52 @@ import ScoreCard from './components/ScoreCard';
 import DiceCard from './components/DiceCard';
 
 function App() {
+  const [playerScore, setPlayerScore] = useState([0, 0]);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [diceScore, setDiceScore] = useState(1);
+  const [roundScore, setRoundScore] = useState(0);
+
+  const reset = () => {
+    setPlayerScore([0, 0]);
+    setCurrentPlayer(0);
+    setDiceScore(1);
+    setRoundScore(0);
+  };
+
+  const roll = () => {
+    const newDiceScore = Math.ceil(Math.random() * 6);
+    setDiceScore(newDiceScore);
+    if (newDiceScore === 1) {
+      setRoundScore(0);
+      setCurrentPlayer(cur => cur === 0 ? 1 : 0);
+    } else {
+      setRoundScore(value => value + newDiceScore);
+    }
+  };
+
+  const keep = () => {
+    setRoundScore(0);
+    setCurrentPlayer(cur => cur === 0 ? 1 : 0);
+    setPlayerScore(value => value.map((ele, idx) => idx === currentPlayer ? ele + roundScore : ele));
+  };
+
   return (
     <Fragment>
-      <Header />
+      <Header reset={reset} />
       <div className="container">
-        <ScoreCard />
-        <DiceCard />
-        <ScoreCard />
+        <ScoreCard
+          score={playerScore[0]}
+          name="1"
+          isCurrent={currentPlayer === 0 ? true : false} />
+        <DiceCard
+          diceScore={diceScore}
+          roundScore={roundScore}
+          roll={roll}
+          keep={keep} />
+        <ScoreCard
+          score={playerScore[1]}
+          name="2"
+          isCurrent={currentPlayer === 1 ? true : false} />
       </div>
     </Fragment>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-function Header() {
+function Header({ reset }) {
   return (
     <div className="header">
-      <button className="btn">New Game</button>
+      <button
+        className="btn"
+        onClick={reset}>New Game</button>
     </div>
   );
 }
